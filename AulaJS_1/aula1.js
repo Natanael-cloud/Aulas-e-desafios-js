2039ee58264d2b90ec3fa777203e97a39be9df35
Natanael-cloud <natanpaivacosta@gmail.com>
2025-07-29 20:28:41 -0300

    entrega final desafio

--- AulaJS_1/aula1.js
+++ AulaJS_1/aula1.js
@@ -1,3 +1,13 @@
+/*exercicio:
+calcular preço de uma viagem */
 
+const priceGas = 4.88;
+const  kmperLiter = 11;
+const  kmDistance = 1777;
 
-console.log("Imprimir algo")
+const  litersConsumed = kmDistance / kmperLiter;
+const  cost = litersConsumed * priceGas;
+
+console.log(cost.toFixed(2));
+
+/*resultado no terminal: 788.34 */
