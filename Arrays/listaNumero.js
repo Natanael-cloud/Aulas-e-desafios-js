3645b78acb3ce22138a4a2129cd87a35e7accd5e
Natanael-cloud <natanpaivacosta@gmail.com>
2025-08-05 19:52:46 -0300

    add code lista numero

---
+++ Arrays/listaNumero.js
@@ -0,0 +1,26 @@
+/*
+Percorre uma lista de numero e imprimi cada numero encontrado
+*/
+
+const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < numeros.length; i++) {
+    const numero = numeros[i];
+
+    if (numero % 2 === 0) {
+        console.log(numero);
+    }
+}
+
+/*
+O retorno no terminal será
+> 
+0
+2
+4
+6
+8
+10
+
+[Done] exited with code=0 in 0.215 seconds
+ */
