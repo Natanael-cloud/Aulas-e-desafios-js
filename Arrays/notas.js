9c0ebfe4b0fd384cf0bf2b1517543dcf3db5078b
Natanael-cloud <natanpaivacosta@gmail.com>
2025-08-05 09:39:36 -0300

    add primeiro exercicio notas usando arrays e metodo push

---
+++ Arrays/notas.js
@@ -0,0 +1,16 @@
+
+
+const notas = [];
+
+notas.push(4); //metodo .push adiciona itens a lista/array declarada
+notas.push(9);
+notas.push(6);
+notas.push(4);
+notas.push(6);
+
+const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
+console.log(soma / 5);
+
+
+
+
