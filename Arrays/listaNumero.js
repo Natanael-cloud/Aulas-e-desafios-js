7e11ee96e7c8f05bb55ad3bbeb4dcc692e8faf2e
Natanael-cloud <natanpaivacosta@gmail.com>
2025-08-05 19:53:25 -0300

    correção comentario

--- Arrays/listaNumero.js
+++ Arrays/listaNumero.js
@@ -1,5 +1,5 @@
 /*
-Percorre uma lista de numero e imprimi cada numero encontrado
+Percorre uma lista de numero e imprimi cada numero par encontrado
 */
 
 const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
