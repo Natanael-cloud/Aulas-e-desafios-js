db4a4a43c2267169f386869ccb83e14c6b69b04d
Natanael-cloud <natanpaivacosta@gmail.com>
2025-08-05 19:22:20 -0300

    add code tabuada com for

---
+++ Arrays/tabuada.js
@@ -0,0 +1,22 @@
+
+const numero = 5;
+for (let i = 1; i <= 10; i++) {
+    console.log(i * numero);
+}
+
+/*
+O retorno no terminal será
+
+3
+6
+9
+12
+15
+18
+21
+24
+27
+30
+
+[Done] exited with code=0 in 0.449 seconds
+*/
